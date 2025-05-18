84325e806a9ba71b03cf68e271662df178e6cf7f
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2025-05-18 15:58:44 +0800

    Initial commit

---
+++ lua/blink-cmp-sshconfig/init.lua
@@ -0,0 +1,24 @@
+--- @module 'blink-cmp'
+
+--- @class SshConfigSource : blink.cmp.Source
+--- @field completion_items blink.cmp.CompletionItem[]
+local sshconfig = {}
+
+function sshconfig.new()
+	return setmetatable({ completion_items = {} }, { __index = sshconfig })
+end
+
+function sshconfig:enabled()
+	return vim.bo.filetype == "sshconfig"
+end
+
+function sshconfig:get_completions(_, callback)
+	callback({
+		is_incomplete_forward = false,
+		is_incomplete_backward = false,
+		items = self.completion_items,
+	})
+	return function() end
+end
+
+return sshconfig
