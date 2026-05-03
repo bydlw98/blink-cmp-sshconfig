ce3850acc5c835fa091aeabe91698cb20215d367
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2026-05-03 21:26:29 +0800

    feat: add a fallback method if unable to generate `completion_items.lua`

--- lua/blink-cmp-sshconfig/init.lua
+++ lua/blink-cmp-sshconfig/init.lua
@@ -5,16 +5,38 @@
 local sshconfig = {}
 
 function sshconfig.build()
+	---@param message string
+	local function info(message)
+		vim.notify("[blink-cmp-sshconfig]: " .. message, vim.log.levels.INFO)
+	end
+
+	--- @type string[]
 	local runtime_paths = vim.split(vim.o.runtimepath, ",", { plain = true })
+
+	--- @type string
 	local plugin_dir = vim.iter(runtime_paths):find(function(path)
 		return vim.endswith(path, "blink-cmp-sshconfig")
 	end)
 
-	vim.system({ "make" }, { cwd = plugin_dir }):wait()
-	vim.notify(
-		"[blink-cmp-sshconfig]: `completion_items.lua` has been generated",
-		vim.log.levels.INFO
-	)
+	local success, result = pcall(function()
+		return vim.system({ "make" }, { cwd = plugin_dir }):wait()
+	end)
+
+	-- Use pre-generated `completion_items.lua` as a fallback if vim.system
+	-- throws an error or a non-zero exit code is returned.
+	if (not success) or result.code ~= 0 then
+		info(
+			"unable to generate `completion_items.lua`, using pre-generated `completion_items.lua` instead"
+		)
+
+		local src_path = plugin_dir .. "/lua/blink-cmp-sshconfig/pre_generated_completion_items.lua"
+		local dest_path = plugin_dir .. "/lua/blink-cmp-sshconfig/completion_items.lua"
+		vim.uv.fs_copyfile(src_path, dest_path)
+
+		info("`pre_generated_completion_items.lua` has been copied over as `completion_items.lua`")
+	else
+		info("`completion_items.lua` has been generated successfully")
+	end
 end
 
 function sshconfig.new()
