0b41352e0f1fe0f19aecb22e9a303894735d9d5a
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2026-05-03 22:27:59 +0800

    feat: add option to prefer using pre-generated `completion_items.lua`

--- lua/blink-cmp-sshconfig/init.lua
+++ lua/blink-cmp-sshconfig/init.lua
@@ -1,13 +1,26 @@
 --- @module 'blink-cmp'
 
+--- @class blink-cmp-sshconfig.Options
+--- @field prefer_pre_generated? boolean use pre-generated `completion_items.lua` instead of generating `completion_items.lua`
+
 --- @class SshConfigSource : blink.cmp.Source
 --- @field completion_items blink.cmp.CompletionItem[]
 local sshconfig = {}
 
-function sshconfig.build()
+--- @param prefer_pre_generated boolean use pre-generated `completion_items.lua` instead of generating `completion_items.lua`
+function sshconfig.build(prefer_pre_generated)
 	---@param message string
 	local function info(message)
 		vim.notify("[blink-cmp-sshconfig]: " .. message, vim.log.levels.INFO)
+	end
+
+	--- @param plugin_dir string
+	local function use_pre_generated_completion_items(plugin_dir)
+		local src_path = plugin_dir .. "/lua/blink-cmp-sshconfig/pre_generated_completion_items.lua"
+		local dest_path = plugin_dir .. "/lua/blink-cmp-sshconfig/completion_items.lua"
+		vim.uv.fs_copyfile(src_path, dest_path)
+
+		info("`pre_generated_completion_items.lua` has been copied over as `completion_items.lua`")
 	end
 
 	--- @type string[]
@@ -18,31 +31,40 @@ function sshconfig.build()
 		return vim.endswith(path, "blink-cmp-sshconfig")
 	end)
 
-	local success, result = pcall(function()
-		return vim.system({ "make" }, { cwd = plugin_dir }):wait()
-	end)
-
-	-- Use pre-generated `completion_items.lua` as a fallback if vim.system
-	-- throws an error or a non-zero exit code is returned.
-	if (not success) or result.code ~= 0 then
-		info(
-			"unable to generate `completion_items.lua`, using pre-generated `completion_items.lua` instead"
-		)
-
-		local src_path = plugin_dir .. "/lua/blink-cmp-sshconfig/pre_generated_completion_items.lua"
-		local dest_path = plugin_dir .. "/lua/blink-cmp-sshconfig/completion_items.lua"
-		vim.uv.fs_copyfile(src_path, dest_path)
-
-		info("`pre_generated_completion_items.lua` has been copied over as `completion_items.lua`")
+	if prefer_pre_generated then
+		use_pre_generated_completion_items(plugin_dir)
 	else
-		info("`completion_items.lua` has been generated successfully")
+		local success, result = pcall(function()
+			return vim.system({ "make" }, { cwd = plugin_dir }):wait()
+		end)
+
+		-- Use pre-generated `completion_items.lua` as a fallback if vim.system
+		-- throws an error or a non-zero exit code is returned.
+		if (not success) or result.code ~= 0 then
+			info(
+				"unable to generate `completion_items.lua`, using pre-generated `completion_items.lua` instead"
+			)
+			use_pre_generated_completion_items(plugin_dir)
+		else
+			info("`completion_items.lua` has been generated successfully")
+		end
 	end
 end
 
-function sshconfig.new()
+--- @param opts? blink-cmp-sshconfig.Options
+function sshconfig.new(opts)
+	if vim.fn.has("nvim-0.11") == 1 then
+		vim.validate("opts", opts, "table", true, "blink-cmp-sshconfig.Options")
+	end
+	opts = opts or {}
+
+	if opts.prefer_pre_generated == nil then
+		opts.prefer_pre_generated = true
+	end
+
 	local has_completion_items, _ = pcall(require, "blink-cmp-sshconfig.completion_items")
 	if not has_completion_items then
-		sshconfig.build()
+		sshconfig.build(opts.prefer_pre_generated)
 	end
 
 	return setmetatable(
