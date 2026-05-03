8e1216605f3177aefbdf693ddd5746390448d725
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2026-05-03 20:23:39 +0800

    feat: add lua api to generate `completion_items.lua`

--- lua/blink-cmp-sshconfig/init.lua
+++ lua/blink-cmp-sshconfig/init.lua
@@ -4,6 +4,19 @@
 --- @field completion_items blink.cmp.CompletionItem[]
 local sshconfig = {}
 
+function sshconfig.build()
+	local runtime_paths = vim.split(vim.o.runtimepath, ",", { plain = true })
+	local plugin_dir = vim.iter(runtime_paths):find(function(path)
+		return vim.endswith(path, "blink-cmp-sshconfig")
+	end)
+
+	vim.system({ "make" }, { cwd = plugin_dir }):wait()
+	vim.notify(
+		"[blink-cmp-sshconfig]: `completion_items.lua` has been generated",
+		vim.log.levels.INFO
+	)
+end
+
 function sshconfig.new()
 	return setmetatable(
 		{ completion_items = require("blink-cmp-sshconfig.completion_items") },
